5032f4a7c6734945f3892e5c19af6dc186a7cf32
FelixMJL <felix_liu03@hotmail.com>
2024-02-23 00:41:03 +1100

    minor fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
-import ClientOnly from "./components/ClientOnly";
-import Container from "./components/Container";
-import EmptyState from "./components/EmptyState";
+import Container from "@/app/components/Container";
+import ListingCard from "@/app/components/listings/ListingCard";
+import EmptyState from "@/app/components/EmptyState";
+
 import getListings, { IListingsParams } from "@/app/actions/getListings";
-import ListingCard from "./components/listings/ListingCard";
-import getCurrentUser from "./actions/getCurrentUser";
+import getCurrentUser from "@/app/actions/getCurrentUser";
+import ClientOnly from "./components/ClientOnly";
 
 interface HomeProps {
   searchParams: IListingsParams;
@@ -26,16 +27,16 @@ const Home = async ({ searchParams }: HomeProps) => {
       <Container>
         <div
           className="
-          pt-24
-          grid
-          grid-cols-1
-          sm:grid-cols-2
-          md:grid-cols-3
-          lg:grid-cols-4
-          xl:grid-cols-5
-          2xl:grid-cols-6
-          gap-8
-        "
+            pt-24
+            grid 
+            grid-cols-1 
+            sm:grid-cols-2 
+            md:grid-cols-3 
+            lg:grid-cols-4
+            xl:grid-cols-5
+            2xl:grid-cols-6
+            gap-8
+          "
         >
           {listings.map((listing: any) => (
             <ListingCard
